f57567a5197667b2c2fa6852afa84d08dc131be0
fengtianyu <fengtianyu@bytedance.com>
2020-12-22 20:26:04 +0800

    Initial Commit

---
+++ OneThing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneThing
+//
+//  Created by fengtianyu on 2020/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OneThing/OneThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneThingApp.swift
+//  OneThing
+//
+//  Created by fengtianyu on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OneThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
